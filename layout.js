a24add35f2ecc836ce58aa6d20a5408083ff4b5b
eyadkhairy <ahmedus77171@gmail.com>
2025-07-29 21:11:24 +0300

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,23 @@
+﻿
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'EYAD KHAIRY - Portfolio',
+  description: 'Video Editor & Content Creator Portfolio',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+      </head>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  )
+}
